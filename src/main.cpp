434c8993498d8fe2e708f1e14a4800e94da098d1
Rafael Pedruzzi <rafaelbelmockpedruzzi@hotmail.com>
2018-11-27 12:54:38 -0200

    Not working

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,6 @@
 
 #include <iostream>
 #include <string>
-// #include "../include/coalition.h"
 #include "../include/election.h"
 #include "../include/reader.h"
 
@@ -26,44 +25,6 @@ int main(int argc, char const *argv[])
 
     Election *election = Reader::readFile(filepath, encoding);
 
-    // delete election;
-
-    // Currently only tests:
-
-    // string b = "BOLO";
-    // string pp = "PQP / PNSC";
-
-    // map<string, Coalition *> *coalitions = new map<string, Coalition *>();
-    // Coalition *temp, *temp2;
-
-    // Coalition coalition1, coalition2;
-    // coalition1.setName(pp);
-    // coalition2.setName(b);
-
-    // Testando um trecho de reader
-
-    // temp = Funcao(coalitions, pp);
-    // temp2 = Funcao(coalitions, b);
-
-    // Party *pqp = new Party("PQP", *temp);
-    // Party *pnsc = new Party("PNSC", *temp);
-    // Party *bolo = new Party("BOLO", *temp2);
-
-    // Candidate *c1 = new Candidate("Alan Herculano Diniz", *pqp, 100, "0.3", true);
-    // Candidate *c2 = new Candidate("Rafael Belmock Pedruzzi", *pnsc, 100, "0.3", true);
-    // Candidate *cenoura = new Candidate("Bolo de Cenoura", *bolo, 350, "25%", false);
-
-    // temp->addCandidate(*c1);
-    // temp->addCandidate(*c2);
-    // temp2->addCandidate(*cenoura);
-
-    // for (map<string, Coalition *>::iterator coIt = coalitions->begin(); coIt != coalitions->end(); coIt++)
-    // {
-    //     cout << coIt->second->getName() << endl << coIt->second->toString() << endl;
-    // }
-
-    // Election *election = new Election(coalitions,2);
-
     cout << election->numberOfVacancies() << endl; // Printing the number of vacant positions
     cout << election->electedCandidates() << endl; // Printing the elected candidates
     cout << election->mostVotedCandidates() << endl; // Printing the most voted candidates
@@ -73,31 +34,7 @@ int main(int argc, char const *argv[])
     cout << election->votesByParty() << endl; // Printing the amount of votes for every party
     cout << "Total de votos nominais: " << election->amountOfVotes() << endl; // Printing the total of nominal votes
 
-    // for (map<string, Coalition *>::iterator coIt = coalitions->begin(); coIt != coalitions->end(); coIt++)
-    // {
-    //     cout << coIt->second->toString() << endl;
-    // }
-
-    // cout << temp->toString() << endl;
-    // cout << temp2->toString() << endl;
-
-    // for (map<string, Coalition *>::iterator coIt = coalitions->begin(); coIt != coalitions->end(); coIt++)
-    // {
-    //     delete coIt->second;
-    // }
-    // delete coalitions;
-
     delete election;
 
     return 0;
-}
-
-Coalition *Funcao(map<string, Coalition *> *coalitions, string s)
-{
-    if (coalitions->find(s) == coalitions->end())
-    {
-        (*coalitions)[s] = new Coalition();
-        (*coalitions)[s]->setName(s);
-    }
-    return coalitions->find(s)->second;
 }
--- src/reader.cpp
+++ src/reader.cpp
@@ -19,8 +19,6 @@
 
 Election *Reader::readFile(const string filePath, const string encoding)
 {
-    cout << "Path: " << filePath << endl << "Encoding: " << encoding << endl;
-
     map<string, Coalition *> *coalitions = new map<string, Coalition *>; // Map with all coalitions.
     bool elected;                                                        // Mark elected candidates.
     string aux, name, coalitionName, partyName, percent;                 // Auxiliar reading variables.
@@ -31,101 +29,97 @@ Election *Reader::readFile(const string filePath, const string encoding)
     Candidate *cand;
     ifstream in;
 
-    cout << "Starting locale..." << endl;
-
-    // setlocale(LC_ALL, "");
-    aux = "pt_BR." + encoding;
-    cout << "Aux = " << aux << endl;
-    locale brasilLocale(aux);
-    aux.clear();
-
-    cout << "Abrindo arquivo..." << endl;
+    locale brasilLocale("pt_BR." + encoding);
 
     in.open(filePath);
-    //in.imbue(brasilLocale);
     getline(in, aux); // Skipping the header.
 
     // while (getline(in, aux))
-    getline(in, aux);
     // {
-    istringstream line(aux);
-    line.imbue(brasilLocale);
+        istringstream line(aux);
+        line.imbue(brasilLocale);
 
-    getline(line, aux, ';'); // Getting the identification number.
-    if (aux.find("#"))       // Break the loop if the section of valid candidates end.
-    {
-        // break;
-    }
-    if (aux.find("*")) // Incrementing the number of vacancies if an elected candidate is found.
-    {
-        vacancies++;
-        elected = true; // Marking the candidate as elected.
-    }
-    else // Else, marking candidate as non elected.
-        elected = false;
-
-    getline(line, aux, ';');  // Skipping the candidate's number.
-    getline(line, name, ';'); // Getting the candidate's name.
-    aux.clear();
-
-    // getline(line, partyName, ';');
-    // Reader::trim(partyName);
-
-    getline(line, aux, ';'); // Getting party and coalition's name.
-
-    std::size_t pos = aux.find("-");
-    if (pos != string::npos)
-    {
-        stringstream pcstream(aux);
-        pcstream >> partyName;
-        pcstream >> coalitionName;  // Skipping '-'.
-        getline(pcstream, coalitionName, ';');  // Getting coalition's name.
-        // coalitionName = aux.substr(++pos); 
-        Reader::trim(coalitionName);
-    }
-    else // If there's no coalition, both names will be seted as the party's name.
-    {
-        partyName = aux;
-        coalitionName = partyName;
-    }
-
-    // line >> votes; // Getting the candidate's votes.
-    // line.ignore(1, ';');
-    getline(line, aux);
-    istringstream vote(aux);
-    vote.imbue(brasilLocale);
-    vote >> votes;
-    getline(line, percent); // Getting the candidate's percent of votes.
-
-    cout << "Name: " << name << " Party: " << partyName << " Coalition: " << coalitionName << " Votes: " << votes << " Percent: " << percent << endl;
-
-    // Getting / Setting the candidate's coalition:
-    if (coalitions->find(coalitionName) == coalitions->end()) // Checking if the coalition already exists.
-    {
-        (*coalitions)[coalitionName] = new Coalition(); // If not, a new one is created.
-        (*coalitions)[coalitionName]->setName(coalitionName);
-    }
-    coalition = coalitions->find(coalitionName)->second;
-
-    // Getting / Setting the candidate's party:
-    for (Party *p : coalition->getParties())
-    {
-        if (p->getName() == partyName) // Checking if the party already exists.
+        getline(line, aux, ';'); // Getting the identification number.
+        if (aux.find("#"))       // Break the loop if the section of valid candidates end.
         {
-            party = p;
-            break;
+            // break;
         }
-        else // If not, a new one is created.
+        if (aux.find("*")) // Incrementing the number of vacancies if an elected candidate is found.
         {
-            party = new Party(partyName, *coalition);
-            break;
+            vacancies++;
+            elected = true; // Marking the candidate as elected.
         }
-    }
+        else // Else, marking candidate as non elected.
+            elected = false;
 
-    cand = new Candidate(name, *party, votes, percent, elected); // Creating this line candidate.
+        getline(line, aux, ';');  // Skipping the candidate's number.
+        getline(line, name, ';'); // Getting the candidate's name.
+        aux.clear();
 
-    coalition->addCandidate(*cand); // Adding the candidate to the coalition.
+        // getline(line, partyName, ';');
+        // Reader::trim(partyName);
+
+        getline(line, aux, ';'); // Getting party and coalition's name.
+
+        std::size_t pos = aux.find("-");
+        if (pos != string::npos)
+        {
+            stringstream pcstream(aux);
+            pcstream >> partyName;
+            pcstream >> coalitionName;  // Skipping '-'.
+            getline(pcstream, coalitionName, ';');  // Getting coalition's name.
+            // coalitionName = aux.substr(++pos); 
+            Reader::trim(coalitionName);
+        }
+        else // If there's no coalition, both names will be seted as the party's name.
+        {
+            partyName = aux;
+            coalitionName = partyName;
+        }
+
+        line >> votes; // Getting the candidate's votes.
+        line.ignore(1, ';');
+        // getline(line, aux);
+        // istringstream vote(aux);
+        // vote.imbue(brasilLocale);
+        // vote >> votes;
+        getline(line, percent); // Getting the candidate's percent of votes.
+        // line >> percent;
+
+        cout << "Name: " << name << " Party: " << partyName << " Coalition: " << coalitionName << " Votes: " << votes << " Percent: " << percent << endl;
+
+        // Getting / Setting the candidate's coalition:
+        cout << "Getting coalition..." << endl;
+        if (coalitions->find(coalitionName) == coalitions->end()) // Checking if the coalition already exists.
+        {
+            (*coalitions)[coalitionName] = new Coalition(); // If not, a new one is created.
+            (*coalitions)[coalitionName]->setName(coalitionName);
+        }
+        coalition = coalitions->find(coalitionName)->second;
+
+        cout << "Getting party..." << endl;
+        // Getting / Setting the candidate's party:
+        for (Party *p : coalition->getParties())
+        {
+            if (p->getName() == partyName) // Checking if the party already exists.
+            {
+                party = p;
+                break;
+            }
+            else // If not, a new one is created.
+            {
+                party = new Party(partyName, *coalition);
+                break;
+            }
+        }
+
+        cout << "Creating candidate..." << endl;
+        cand = new Candidate(name, *party, votes, percent, elected); // Creating this line candidate.
+
+        cout << "Adding candidate..." << endl;
+        coalition->addCandidate(*cand); // Adding the candidate to the coalition.
     // }
+    cout << "Returning..." << endl;
     return new Election(coalitions, vacancies);
 }
 
